ee8fcf12941cc81be6d57bff3adb2b0e31eac6ae
johh <code@johh.net>
2021-07-22 13:54:05 +0200

    refactor: remove scheduler dependency
    BREAKING CHANGE: scheduler must now be added manually

--- src/lib/defaultWorld.ts
+++ src/lib/defaultWorld.ts
@@ -1,12 +1,6 @@
-import { loop, update } from '@downpourdigital/scheduler';
-
 import World from './World';
 
 
 const defaultWorld = new World();
-
-loop( () => [
-	update( ( delta, time ) => defaultWorld.step( time ) ),
-]);
 
 export default defaultWorld;
